8d2eb1dd777f9fa7fe37bab524f20673b332dee5
reactdev-develpify <117104256+reactdev-develpify@users.noreply.github.com>
2022-11-08 18:37:59 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Registration from "./components/Registration";
+
+function App() {
+  return (
+    <main className="App">
+      <Registration />
+    </main>
+  );
+}
+
+export default App;
